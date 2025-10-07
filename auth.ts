8c14380f7a93a44d4cf76c164d43537879f7f1b6
Arif Khan <arif@example.com>
2025-10-07 17:24:29 +0530

    fix: restore db client and nextauth handlers exports

--- auth.ts
+++ auth.ts
@@ -2,4 +2,11 @@ import NextAuth from 'next-auth';
 
 import { authConfig } from './auth.config';
 
-export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
+const authResponse = NextAuth(authConfig);
+
+export const auth = authResponse.auth;
+export const signIn = authResponse.signIn;
+export const signOut = authResponse.signOut;
+export const handlers = authResponse.handlers;
+export const GET = authResponse.handlers.GET;
+export const POST = authResponse.handlers.POST;
